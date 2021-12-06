4f877063fe1f34778e387944eb0080893ef10305
Thisara Malintha <thisara.malintha@hotmail.com>
2021-12-07 02:04:44 +0530

    Add project files.

---
+++ GameBarTimer/Components/Timer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml;
+
+namespace GameBarTimer.Components
+{
+    public static class Timer
+    {
+        private static readonly string TIMER_DURATION_SETTING_KEY = "timerDuration";
+        private static readonly string TIMER_SUSPEND_TIMESTAMP_SETTINGS_KEY = "timerSuspendTimestamp";
+
+        private static DispatcherTimer _dispatcherTimer;
+        private static int _durationInSeconds;
+        private static int _remainingHours;
+        private static int _remainingMinutes;
+        private static int _remainingSeconds;
+
+        public static event Action<int, int, int> OnSecondElapse;
+        public static event Action OnTimerStop;
+        public static event Action OnTimerFinish;
+
+        static Timer()
+        {
+            Application.Current.Suspending += App_Suspending;
+            Application.Current.Resuming += App_Resuming;
+        }
+
+        private static void App_Resuming(object sender, object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void App_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            ApplicationData.Current.LocalSettings.Values[TIMER_DURATION_SETTING_KEY] = _durationInSeconds;
+            ApplicationData.Current.LocalSettings.Values[TIMER_SUSPEND_TIMESTAMP_SETTINGS_KEY] = DateTime.Now.Ticks;
+        }
+
+        public static void Start(int hours, int minutes)
+        {
+            CreateDispatcherTimerIfNotExists();
+
+            _durationInSeconds = hours * 3600 + minutes * 60;
+            _remainingHours = hours;
+            _remainingMinutes = minutes;
+            _remainingSeconds = 59;
+
+            _dispatcherTimer.Start();
+        }
+
+        public static void Stop()
+        {
+            _dispatcherTimer.Stop();
+            Application.Current.Suspending -= App_Suspending;
+            Application.Current.Resuming -= App_Resuming;
+        }
+
+        private static void CreateDispatcherTimerIfNotExists()
+        {
+            if (_dispatcherTimer == null)
+            {
+                _dispatcherTimer = new DispatcherTimer()
+                {
+                    Interval = TimeSpan.FromSeconds(1)
+                };
+                _dispatcherTimer.Tick += DispatcherTimer_Tick;
+            }
+        }
+
+        private static void DispatcherTimer_Tick(object sender, object e)
+        {
+            _remainingSeconds = Math.Max(_remainingSeconds - 1, 0);
+
+            if (_remainingSeconds == 0 && _remainingMinutes == 0 && _remainingHours == 0)
+            {
+                FinishTimer();
+                return;
+            }
+
+            if (_remainingSeconds == 0)
+            {
+                _remainingMinutes = Math.Max(_remainingMinutes - 1, 0);
+
+                if (_remainingMinutes == 0)
+                {
+                    _remainingHours = Math.Max(_remainingHours - 1, 0);
+                    _remainingMinutes = 59;
+                }
+
+                _remainingSeconds = 59;
+            }
+
+            OnSecondElapse.Invoke(_remainingHours, _remainingMinutes, _remainingSeconds);
+        }
+
+        private static void FinishTimer()
+        {
+            _dispatcherTimer.Stop();
+            OnTimerFinish.Invoke();
+        }
+    }
+}
---
+++ GameBarTimer/MainPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿using GameBarTimer.Components;
+using Microsoft.Gaming.XboxGameBar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GameBarTimer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private string _countdownHours;
+        public string CountdownHours
+        {
+            get { return _countdownHours; }
+            set
+            {
+                _countdownHours = value;
+                RaiseNotifyPropertyChanged();
+            }
+        }
+
+        private string _countdownMinutes;
+        public string CountdownMinutes
+        {
+            get { return _countdownMinutes; }
+            set
+            {
+                _countdownMinutes = value;
+                RaiseNotifyPropertyChanged();
+            }
+        }
+
+        private string _countdownSeconds;
+        public string CountdownSeconds
+        {
+            get { return _countdownSeconds; }
+            set
+            {
+                _countdownSeconds = value;
+                RaiseNotifyPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Timer.OnSecondElapse += Timer_OnSecondElapse;
+        }
+
+        private void Timer_OnSecondElapse(int remainingHours, int remainingMinutes, int remainingSeconds)
+        {
+            CountdownHours = remainingHours.ToString();
+            CountdownMinutes = remainingMinutes.ToString().PadLeft(2, '0');
+            CountdownSeconds = remainingSeconds.ToString().PadLeft(2, '0');
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null)
+            {
+                var widget = e.Parameter as XboxGameBarWidget;
+                if (widget != null)
+                    widget.VisibleChanged += Widget_VisibleChanged;
+            }
+        }
+
+        private void Widget_VisibleChanged(XboxGameBarWidget sender, object args)
+        {
+            Debug.WriteLine("Visibility is " + sender.Visible);
+
+        }
+
+        public void StartTimerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(HourInputTextBox.Text, out int hours);
+            int.TryParse(MinuteInputTextBox.Text, out int minutes);
+
+            Timer.Start(hours, minutes);
+
+            TimerInputPanel.Visibility = Visibility.Collapsed;
+            CountDownStatusPanel.Visibility = Visibility.Visible;
+
+            StatusTextBlock.Text = "Running";
+        }
+
+        private void RaiseNotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
